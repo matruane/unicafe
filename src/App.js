3630dff1a1b5d689d8d13651750475a86f0423b2
Mat Ruane <mat@ruane.net.nz>
2021-11-20 15:15:17 +1300

    declare props explicitly for Display component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Display = (props) => {
+const Display = ({ good, neutral, bad}) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
